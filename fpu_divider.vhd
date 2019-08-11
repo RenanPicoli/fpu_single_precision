c334c9ef8ed6ae7798d0a34079a29d1d115e8e8e
RenanPicoli <renanps123@gmail.com>
2019-08-11 20:43:02 -0300

    removed commented code

--- fpu_divider.vhd
+++ fpu_divider.vhd
@@ -84,9 +84,9 @@ process(A,B,A_fp,B_fp,res_expanded_mantissa)
 	
 	--overflow/underflow detection. See ovflw_undflw.txt for explanation
 	res_exp_aux := std_logic_vector(to_unsigned(res_exp,9));
+	--overflow/underflow handling
 	overflow_aux := res_exp_aux(8) and (not res_exp_aux(7));
 	underflow_aux := res_exp_aux(8) and  res_exp_aux(7);
-	--overflow/underflow handling
 	overflow <= overflow_aux;
 	underflow<= underflow_aux;
 	
@@ -110,36 +110,17 @@ process(A,B,A_fp,B_fp,res_expanded_mantissa)
 
 	-- division: normal case
 	else
---		A_exp:= to_integer(unsigned(A_fp.exponent));
---		B_exp:= to_integer(unsigned(B_fp.exponent));
---		res_exp := A_exp - B_exp + EXP_BIAS;--this int varies from 0-255+127=-128=1'1000'0000 to 255-0+127=382=1'0111'1110
---		
---		--overflow/underflow detection. See ovflw_undflw.txt for explanation
---		res_exp_aux := std_logic_vector(to_unsigned(res_exp,9));
-----		overflow_aux := res_exp_aux(8) and (not res_exp_aux(7));
-----		underflow_aux := res_exp_aux(8) and  res_exp_aux(7);
-		
 		-- normalization: only 2 cases: C0C1=1X or C0C1=01
 		if res_expanded_mantissa(23)='1' then--no need for normalization
 			result <= (A_fp.sign xor B_fp.sign) & std_logic_vector(to_unsigned(res_exp,8)) & res_expanded_mantissa(22 downto 0);
 		else--C(0)='0' but C(1)='1'
 			result <= (A_fp.sign xor B_fp.sign) & std_logic_vector(to_unsigned(res_exp-1,8)) & res_expanded_mantissa(21 downto 0) & '0';
 		end if;
-		
---		--overflow/underflow handling
---		overflow <= overflow_aux;
---		underflow<= underflow_aux;
+
 		if(overflow_aux='1') then--result is set to +/-Inf
 			result <= (A_fp.sign xor B_fp.sign) & positive_Inf(30 downto 0);
---			overflow <='1';
---			underflow <='0';
 		elsif (underflow_aux='1') then--result is set to +/-0
 			result <= (A_fp.sign xor B_fp.sign) & positive_zero(30 downto 0);
---			underflow<='1';
---			overflow<='0';
---		else
---			overflow <= '0';
---			underflow<='0';
 		end if;
 	end if;
 	
