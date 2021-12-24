4f77c8f315dd6a70d35518dda75ca3f11fdf9d7d
RenanPicoli <renanps123@gmail.com>
2021-12-24 18:23:59 -0300

    fpu_test.m, testbench_fpu.vhd: changed to test floating point division

--- testbench_fpu.vhd
+++ testbench_fpu.vhd
@@ -43,8 +43,8 @@ begin
 	
 --	op <= "00";--add numbers
 --	op <= "01";--subtract numbers
-	op <= "10";--multiply numbers
---	op <= "11";--divide numbers
+--	op <= "10";--multiply numbers
+	op <= "11";--divide numbers
 	
 	-----------------------------------------------------------
 	--	this process reads a file vector, loads its vectors,
